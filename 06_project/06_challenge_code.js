d6cf3e020ee3301099f422aa81435cb452d0a980
mariaclaudianicolai <mariaclaudia.nicolai@gmail.com>
2024-07-04 13:43:41 +0200

    first challenge using strings

---
+++ 06_project/06_challenge_code.js
@@ -0,0 +1,11 @@
+// Code Challenge
+
+// Write JavaScript code that will return a random letter from your name
+
+// Let's make this work for all names
+const anyName = "Jonathan"
+
+console.log(anyName.charAt(Math.floor(Math.random() * anyName.length)))
+console.log(anyName.charAt(Math.floor(Math.random() * anyName.length)))
+console.log(anyName.charAt(Math.floor(Math.random() * anyName.length)))
+
